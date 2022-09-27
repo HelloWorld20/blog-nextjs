8261b3a16c1956f8de9ff4dba21cfea50425c138
Leon.W <weijianghong@tuzhanai.com>
2022-09-27 14:41:02 +0800

    feat: ✨  展开试导航半成品、引入iconfont

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -3,6 +3,7 @@ import DateFormatter from './date-formatter'
 import CoverImage from './cover-image'
 import Link from 'next/link'
 import type Author from '../interfaces/author'
+import Hitokoto from './hitokoto'
 
 type Props = {
   title: string
@@ -18,11 +19,40 @@ const HeroPost = ({
   coverImage,
   date,
   excerpt,
-  author,
   slug,
 }: Props) => {
   return (
     <section>
+      <div className='absolute top-10 right-10 z-10 text-xs'>
+        <div className='absolute right-10 bg-gray-50 p-1.5 rounded-sm cursor-pointer hover:bg-gray-100'>
+          <i className='iconfont icon-search' style={{ lineHeight: '22px' }}></i>
+        </div>
+        <div className='absolute right-0 bg-gray-50 p-1.5 rounded-sm cursor-pointer hover:bg-gray-100'>
+          <i className='iconfont icon-menu' style={{ lineHeight: '22px' }}></i>
+        </div>
+      </div>
+
+      <div className='w-screen h-screen bg-gray-50 bg-opacity-25'>
+        <div className='absolute top-10 right-10 z-20 text-xs'>
+          <div className='absolute right-10 bg-gray-50 p-1.5 rounded-sm cursor-pointer hover:bg-gray-100'>
+            <i className='iconfont icon-search' style={{ lineHeight: '22px' }}></i>
+          </div>
+          <div className='absolute right-0 bg-gray-50 p-1.5 rounded-sm cursor-pointer hover:bg-gray-100'>
+            <i className='iconfont icon-close' style={{ lineHeight: '22px' }}></i>
+          </div>
+        </div>
+        <ul className='pt-36 flex justify-center text-lg text-gray-500 space-x-8'>
+          {
+            ['首页', '标签', '归档', '收藏', '关于'].map(name => (<li key={name}>
+              <a className="cursor-pointer hover:text-gray-800">{name}</a>
+            </li>))
+          }
+        </ul>
+        <div className='text-center text-sm text-gray-400 mt-20'>
+          <Hitokoto />
+        </div>
+
+      </div>
       <div className="mb-8 md:mb-16">
         <CoverImage title={title} src={coverImage} slug={slug} />
       </div>
@@ -39,7 +69,6 @@ const HeroPost = ({
         </div>
         <div>
           <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          {/* <Avatar name={author.name} picture={author.picture} /> */}
         </div>
       </div>
     </section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,19 +20,19 @@ export default function Index({ allPosts }: Props) {
         <Head>
           <title>Leon's Blog</title>
         </Head>
+        {heroPost && (
+          <HeroPost
+            title={heroPost.title}
+            // coverImage={heroPost.coverImage}
+            coverImage={'/assets/blog/preview/cover.jpg'}
+            date={heroPost.date}
+            author={heroPost.author}
+            slug={heroPost.slug}
+            excerpt={heroPost.excerpt}
+          />
+        )}
         <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              // coverImage={heroPost.coverImage}
-              coverImage={'/assets/blog/preview/cover.jpg'}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
+          {/* <Intro /> */}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
