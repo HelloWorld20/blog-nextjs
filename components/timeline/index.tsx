c2f42fb14c8397f61ff02115d20b4cd63433145d
Leon.W <weijianghong@tuzhanai.com>
2022-10-25 15:18:57 +0800

    feat: ✨  添加归档页面

--- components/timeline/index.tsx
+++ components/timeline/index.tsx
@@ -1,14 +1,47 @@
+/**
+ * 时间轴组件
+ * @author jianghong.wei
+ * @since 2022-10-25 15:15:31
+ */
 
-// interface DataItem {
-//   type: 'TITLE' | 'SUB_TITLE' | "ITEM";
-//   time: string;
-//   title: string;
-// }
+interface DataItem {
+  size: number;
+  element: React.ReactElement;
+}
 
-// interface IProps {
-//   data: DataItem;
-// }
+interface IProps {
+  data: DataItem[];
+}
 
-export default function Timeline() {
-  return <div>timeline</div>
+/** 内置尺寸 */
+export enum SIZE {
+  XS = 5,
+  SM = 8,
+  BASE = 12,
+  LG = 15,
+  XL = 18,
+  XXL = 22,
+}
+
+export default function Timeline(props: IProps) {
+  return (
+    <div className="border-l-4 border-gray-300 pl-6 m-5 py-2">
+      {props.data.map(({ element, size }) => {
+        return (
+          <div className="relative mb-6 last:mb-0">
+            <i
+              className="absolute border-gray-300 border-4 rounded-full bg-white box-content"
+              style={{
+                width: `${size}px`,
+                height: `${size}px`,
+                top: '50%',
+                transform: `translate(-50%, -50%)`,
+                left: 'calc(-2px - 1.5rem)',
+              }}></i>
+            {element}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- pages/archives.tsx
+++ pages/archives.tsx
@@ -1,59 +1,78 @@
-import Timeline from "@/components/timeline";
-import { getAllPosts } from "@/lib/api";
-import { useMemo } from "react";
+import Timeline, { SIZE } from '@/components/timeline';
+import { getAllPosts } from '@/lib/api';
+import { useMemo } from 'react';
+import dayjs from 'dayjs';
+import Link from 'next/link'
+
+function getItemTitle(num: number) {
+  return { size: SIZE.LG, element: <h2 className="text-3xl">归档: {num}篇</h2> };
+}
+
+function getItemPost(time: string, slug: string) {
+  return {
+    size: SIZE.SM,
+    element: (
+      <>
+        <time className="text-sm text-gray-400">{time}</time>&nbsp;
+        <Link as={`/posts/${slug}`} href="/posts/[slug]">
+          <a className="hover:underline pl-4 text-sm text-blue-500">{slug}</a>
+        </Link>
+      </>
+    ),
+  };
+}
+
+function getItemYear(year: number) {
+  return {
+    size: SIZE.BASE,
+    element: <h3 className="text-xl">{year}</h3>,
+  };
+}
 
 export default function Archives({ posts }) {
-
   const postsDivideByYear = useMemo(() => {
     const result = posts.slice();
-
-    console.log('%c [ posts ]-15', 'font-size:13px; background:pink; color:#bf2c9f;', posts)
 
     let len = result.length;
     const lastPostData = new Date(result[len - 1].date);
     let lastPostYear = lastPostData.getFullYear();
 
-    // let currentYear = lastPostYear;
-
-    for(let i = len-2; i >= 0; i--) {
+    for (let i = len - 2; i >= 0; i--) {
       const post = result[i];
       const date = new Date(post.date);
       const year = date.getFullYear();
-      if(year !== lastPostYear) {
+      if (year !== lastPostYear) {
         // 插入一个
-        result.splice(i, 0, {flag: true, year})
-        // currentYear = lastPostYear;
+        result.splice(i + 1, 0, { flag: true, year: lastPostYear });
         lastPostYear = year;
       }
     }
 
-
-    console.log('%c [ result ]-28', 'font-size:13px; background:pink; color:#bf2c9f;', result)
+    result.unshift({ flag: true, year: new Date(result[0].date).getFullYear() });
 
     return result;
-
   }, [posts]);
 
   const timelineData = useMemo(() => {
-    return postsDivideByYear.map(item => {
+    const result = postsDivideByYear.map(item => {
       if (item.flag) {
-
+        return getItemYear(item.year);
       }
-    })
-  }, [postsDivideByYear])
+      const time = dayjs(item.date).format('YYYY-MM-DD');
+      return getItemPost(time, item.slug);
+    });
 
-  return <Timeline />;
+    result.unshift(getItemTitle(result.length));
+
+    return result;
+
+  }, [postsDivideByYear]);
+
+  return <Timeline data={timelineData} />;
 }
 
 export function getStaticProps() {
-  const posts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    "author",
-    "coverImage",
-    "excerpt",
-  ]);
+  const posts = getAllPosts(['title', 'date', 'slug', 'author', 'coverImage', 'excerpt']);
 
   return {
     props: { posts },
